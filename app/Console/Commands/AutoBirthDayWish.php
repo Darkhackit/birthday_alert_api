951a97d9797325f5071e1f73ba04548a9ca00aad
Darkhackit <earthur510@gmail.com>
2023-01-10 16:37:26 +0000

    10/01/2023

---
+++ app/Console/Commands/AutoBirthDayWish.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\BirthDayMail;
+use App\Models\Employee;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class AutoBirthDayWish extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'auto:birthdays';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Automatically send birthday wishes to employees';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $employees = Employee::whereMonth('dob',Carbon::now()->format('m'))
+            ->whereDay('dob', Carbon::now()->format('d'))->get();
+        if($employees->count() > 0) {
+            foreach ($employees as $employee) {
+                Mail::to($employee->email)->send(new BirthDayMail($employee));
+            }
+        }
+        return 0;
+    }
+}
---
+++ app/Mail/BirthDayMail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Employee;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class BirthDayMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public Employee $employee;
+    public function __construct(Employee $employee)
+    {
+        $this->employee  = $employee;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('jeffrey@example.com', 'Jeffrey Way'),
+            subject: 'Birth Day Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.birthday',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
